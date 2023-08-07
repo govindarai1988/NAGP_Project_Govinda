4f95b5d61a18091b9cac98f5f336b5f9be9daf2c
Govinda Rai <govinda.rai@nagarro.com>
2023-08-07 14:46:36 +0530

    final commit

--- src/main/java/variables/URL.java
+++ src/main/java/variables/URL.java
@@ -2,6 +2,10 @@ package variables;
 
 public final class URL {
 	
+	 private URL() {
+		    throw new IllegalStateException("Utility class");
+		  }
+	
 	public static final String BASE_URL = "https://opensource-demo.orangehrmlive.com/";
 
 }
